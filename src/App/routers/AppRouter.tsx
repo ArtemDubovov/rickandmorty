081ea47299d5f198ad6432860d628b030867b5ca
Artem Dubovov <artem.dubovov@bk.ru>
2025-12-01 00:29:32 +0300

    feat: added fonts and error page

--- src/App/routers/AppRouter.tsx
+++ src/App/routers/AppRouter.tsx
@@ -1,15 +1,32 @@
 import React from "react";
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Home from "../../pages/Home";
 import CardInfo from "../../pages/CardInfo";
+import Error from "../../pages/Error";
+
+const routers = [
+	{
+		path: "/",
+		element: <Home />,
+	},
+	{
+		path: "/card",
+		element: <CardInfo />,
+	},
+    {
+		path: "/*",
+		element: <Error />,
+	},
+];
 
 function AppRouter() {
     return(
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/card" element={<CardInfo />} />
+                {
+                    routers.map(r => <Route key={r.path} path={r.path} element={r.element}/>)
+                }
             </Routes>
         </BrowserRouter>
     )
